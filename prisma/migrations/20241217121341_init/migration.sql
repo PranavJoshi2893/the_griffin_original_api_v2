406774c4a5d3045379aa98ccb6dfb05d310b8af5
Pranav Joshi <pranavjoshi2893@gmail.com>
2024-12-17 17:50:32 +0530

    first commit

---
+++ prisma/migrations/20241217121341_init/migration.sql
@@ -0,0 +1,96 @@
+-- CreateTable
+CREATE TABLE "product_gender" (
+    "pgid" SERIAL NOT NULL,
+    "gender_name" VARCHAR(10) NOT NULL,
+
+    CONSTRAINT "product_gender_pkey" PRIMARY KEY ("pgid")
+);
+
+-- CreateTable
+CREATE TABLE "product_category" (
+    "pcid" SERIAL NOT NULL,
+    "product_gender_id" INTEGER NOT NULL,
+    "category_name" VARCHAR(100) NOT NULL,
+
+    CONSTRAINT "product_category_pkey" PRIMARY KEY ("pcid")
+);
+
+-- CreateTable
+CREATE TABLE "product" (
+    "pid" SERIAL NOT NULL,
+    "product_name" VARCHAR(500) NOT NULL,
+    "product_category_id" INTEGER NOT NULL,
+
+    CONSTRAINT "product_pkey" PRIMARY KEY ("pid")
+);
+
+-- CreateTable
+CREATE TABLE "product_image" (
+    "piid" SERIAL NOT NULL,
+    "image_filename" VARCHAR(400) NOT NULL,
+    "product_item_id" INTEGER NOT NULL,
+
+    CONSTRAINT "product_image_pkey" PRIMARY KEY ("piid")
+);
+
+-- CreateTable
+CREATE TABLE "colour" (
+    "cid" SERIAL NOT NULL,
+    "color_name" TEXT NOT NULL,
+
+    CONSTRAINT "colour_pkey" PRIMARY KEY ("cid")
+);
+
+-- CreateTable
+CREATE TABLE "product_item" (
+    "piid" SERIAL NOT NULL,
+    "original_price" INTEGER NOT NULL,
+    "sale_price" INTEGER NOT NULL,
+    "product_id" INTEGER NOT NULL,
+    "colour_id" INTEGER NOT NULL,
+
+    CONSTRAINT "product_item_pkey" PRIMARY KEY ("piid")
+);
+
+-- CreateTable
+CREATE TABLE "size_option" (
+    "sid" SERIAL NOT NULL,
+    "size_name" TEXT NOT NULL,
+    "sort_order" INTEGER NOT NULL,
+
+    CONSTRAINT "size_option_pkey" PRIMARY KEY ("sid")
+);
+
+-- CreateTable
+CREATE TABLE "product_variation" (
+    "pvid" SERIAL NOT NULL,
+    "product_item_id" INTEGER NOT NULL,
+    "qty_in_stock" INTEGER NOT NULL,
+    "size_id" INTEGER NOT NULL,
+
+    CONSTRAINT "product_variation_pkey" PRIMARY KEY ("pvid")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "product_gender_gender_name_key" ON "product_gender"("gender_name");
+
+-- AddForeignKey
+ALTER TABLE "product_category" ADD CONSTRAINT "product_category_product_gender_id_fkey" FOREIGN KEY ("product_gender_id") REFERENCES "product_gender"("pgid") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "product" ADD CONSTRAINT "product_product_category_id_fkey" FOREIGN KEY ("product_category_id") REFERENCES "product_category"("pcid") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "product_image" ADD CONSTRAINT "product_image_product_item_id_fkey" FOREIGN KEY ("product_item_id") REFERENCES "product_item"("piid") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "product_item" ADD CONSTRAINT "product_item_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "product"("pid") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "product_item" ADD CONSTRAINT "product_item_colour_id_fkey" FOREIGN KEY ("colour_id") REFERENCES "colour"("cid") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "product_variation" ADD CONSTRAINT "product_variation_product_item_id_fkey" FOREIGN KEY ("product_item_id") REFERENCES "product_item"("piid") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "product_variation" ADD CONSTRAINT "product_variation_size_id_fkey" FOREIGN KEY ("size_id") REFERENCES "size_option"("sid") ON DELETE RESTRICT ON UPDATE CASCADE;
